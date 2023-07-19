7c6804e21a0af8cae0f6e24f256d5489d94df1c3
Adienox <adwaitadk@pm.me>
2023-07-19 11:34:25 +0545

    managing indentation

--- flake/configuration.nix
+++ flake/configuration.nix
@@ -1,9 +1,5 @@
-{ config,lib, pkgs, ... }:
-{
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+{ config, lib, pkgs, ... }: {
+  imports = [ ./hardware-configuration.nix ];
 
   boot = {
     loader = {
@@ -11,7 +7,7 @@
       timeout = 3;
       grub = {
         enable = true;
-        devices = ["nodev"];
+        devices = [ "nodev" ];
         useOSProber = false;
         efiSupport = true;
         configurationLimit = 5;
@@ -20,28 +16,23 @@
   };
 
   environment.variables = {
-      QT_QPA_PLATFORMTHEME = "qt5ct";
-      QT_QPA_PLATFORM = "xcb obs";
-      #https://unix.stackexchange.com/a/657578
-      LIBSEAT_BACKEND = "logind";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    QT_QPA_PLATFORM = "xcb obs";
+    #https://unix.stackexchange.com/a/657578
+    LIBSEAT_BACKEND = "logind";
   };
 
   # Using https://github.com/samuelngs/apple-emoji-linux Apple Color Emoji as the default emoji font. Download and place it in ~$HOME/.local/share/fonts~
   fonts = {
     fontDir.enable = true;
-    fonts = with pkgs; [
-      nerdfonts
-      font-awesome
-      google-fonts
-      source-han-sans
-    ];
+    fonts = with pkgs; [ nerdfonts font-awesome google-fonts source-han-sans ];
     fontconfig.defaultFonts.emoji = [ "Apple Color Emoji" ];
   };
 
   hardware.bluetooth = {
-      enable = true;
-      settings.General.Experimental = true;
-    };
+    enable = true;
+    settings.General.Experimental = true;
+  };
   services.blueman.enable = true;
 
   time.timeZone = "Asia/Kathmandu";
@@ -51,15 +42,21 @@
     hostName = "anomaly";
     networkmanager.enable = true;
 
-    nameservers = ["1.1.1.1"];
-    networkmanager.insertNameservers = ["1.1.1.1"];
+    nameservers = [ "1.1.1.1" ];
+    networkmanager.insertNameservers = [ "1.1.1.1" ];
 
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22000 ];
       allowedUDPPorts = [ ];
-      allowedTCPPortRanges = [ {from = 1714; to = 1764;} ]; # Needed for KDE connect
-      allowedUDPPortRanges = [ {from = 1714; to = 1764;} ]; # Needed for KDE connect
+      allowedTCPPortRanges = [{
+        from = 1714;
+        to = 1764;
+      }]; # Needed for KDE connect
+      allowedUDPPortRanges = [{
+        from = 1714;
+        to = 1764;
+      }]; # Needed for KDE connect
     };
   };
   # Configure network proxy if necessary
@@ -67,7 +64,6 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   services.xserver = {
-    enable = true;
     layout = "us";
     xkbVariant = "";
   };
@@ -76,8 +72,9 @@
     isNormalUser = true;
     description = "adienox";
     shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" "adbusers" "input" ];
-    packages = with pkgs; [];
+    extraGroups =
+      [ "networkmanager" "wheel" "video" "audio" "adbusers" "input" ];
+    packages = with pkgs; [ ];
   };
 
   # Make sure opengl is enabled
@@ -87,7 +84,7 @@
     driSupport32Bit = true;
   };
   # Tell Xorg to use the nvidia driver
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     # Modesetting is needed for most wayland compositors
@@ -158,14 +155,14 @@
   security = {
     polkit.enable = true;
     pam.services.swaylock = {
-        text = ''
+      text = ''
         auth include login
-        '';
+      '';
     };
     pam.services.gtklock = {
-        text = ''
+      text = ''
         auth include login
-        '';
+      '';
     };
     sudo.extraConfig = ''
       Defaults env_reset,pwfeedback
@@ -205,56 +202,54 @@
 
   # Power Management
   boot = {
-      extraModprobeConfig =''
-          options iwlwifi power_save=1
-          options iwlmvm power_scheme=3
-          options snd_hda_intel power_save=1
-          blacklist sp5100_tco
-      '';
-      kernel.sysctl = {
+    extraModprobeConfig = ''
+      options iwlwifi power_save=1
+      options iwlmvm power_scheme=3
+      options snd_hda_intel power_save=1
+      blacklist sp5100_tco
+    '';
+    kernel.sysctl = {
       "kernel.nmi_watchdog" = 0;
       "vm.dirty_writeback_centisecs" = 1500;
       "vm.laptop_mode" = 5;
-      };
+    };
   };
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="net", KERNEL=="wl*", RUN+="${pkgs.iw}/bin/iw dev $name set power_save on"
   '';
-  systemd.tmpfiles.rules = map
-          (e:
-          "w /sys/bus/${e}/power/control - - - - auto"
-          ) [
-            "pci/devices/0000:00:00.0" # Root Complex
-            "pci/devices/0000:00:00.2" # IOMMU
-            "pci/devices/0000:00:01.0" # Renoir PCIe Dummy Host Bridge
-            "pci/devices/0000:00:02.0" # Renoir PCIe Dummy Host Bridge
-            "pci/devices/0000:00:08.0" # Renoir PCIe Dummy Host Bridge
-            "pci/devices/0000:00:14.0" # FCH SMBus Controller
-            "pci/devices/0000:00:14.3" # FCH LPC bridge
-            "pci/devices/0000:00:18.0" # Renoir Function 0
-            "pci/devices/0000:00:18.1" # Renoir Function 1
-            "pci/devices/0000:00:18.2" # Renoir Function 2
-            "pci/devices/0000:00:18.3" # Renoir Function 3
-            "pci/devices/0000:00:18.4" # Renoir Function 4
-            "pci/devices/0000:00:18.5" # Renoir Function 5
-            "pci/devices/0000:00:18.6" # Renoir Function 6
-            "pci/devices/0000:00:18.7" # Renoir Function 7
-            "pci/devices/0000:01:00.0" # Nvidia GPU
-            "pci/devices/0000:02:00.0" # Non-Volatile Memory Controller
-            "pci/devices/0000:03:00.0" # Ethernet
-            "pci/devices/0000:04:00.0" # Wifi
-            "pci/devices/0000:05:00.0" # VGA controller
-            "pci/devices/0000:05:00.2" # Encryption controller
-            "pci/devices/0000:05:00.3" # Renoir/Cezanne USB 3.1
-            "pci/devices/0000:05:00.5" # Audio co-processor
-            "pci/devices/0000:05:00.6" # Audio controller
-            "pci/devices/0000:06:00.0" # FCH SATA Controller [AHCI mode]
-            "pci/devices/0000:06:00.1" # FCH SATA Controller [AHCI mode]
-            "pci/devices/0000:06:00.0/ata1" # FCH SATA Controller [AHCI mode]
-            "pci/devices/0000:06:00.1/ata2" # FCH SATA Controller [AHCI mode]
-            "usb/devices/3-3" # USB device 3-3
-            "usb/devices/3-4" # ITE device
-          ];
+  systemd.tmpfiles.rules =
+    map (e: "w /sys/bus/${e}/power/control - - - - auto") [
+      "pci/devices/0000:00:00.0" # Root Complex
+      "pci/devices/0000:00:00.2" # IOMMU
+      "pci/devices/0000:00:01.0" # Renoir PCIe Dummy Host Bridge
+      "pci/devices/0000:00:02.0" # Renoir PCIe Dummy Host Bridge
+      "pci/devices/0000:00:08.0" # Renoir PCIe Dummy Host Bridge
+      "pci/devices/0000:00:14.0" # FCH SMBus Controller
+      "pci/devices/0000:00:14.3" # FCH LPC bridge
+      "pci/devices/0000:00:18.0" # Renoir Function 0
+      "pci/devices/0000:00:18.1" # Renoir Function 1
+      "pci/devices/0000:00:18.2" # Renoir Function 2
+      "pci/devices/0000:00:18.3" # Renoir Function 3
+      "pci/devices/0000:00:18.4" # Renoir Function 4
+      "pci/devices/0000:00:18.5" # Renoir Function 5
+      "pci/devices/0000:00:18.6" # Renoir Function 6
+      "pci/devices/0000:00:18.7" # Renoir Function 7
+      "pci/devices/0000:01:00.0" # Nvidia GPU
+      "pci/devices/0000:02:00.0" # Non-Volatile Memory Controller
+      "pci/devices/0000:03:00.0" # Ethernet
+      "pci/devices/0000:04:00.0" # Wifi
+      "pci/devices/0000:05:00.0" # VGA controller
+      "pci/devices/0000:05:00.2" # Encryption controller
+      "pci/devices/0000:05:00.3" # Renoir/Cezanne USB 3.1
+      "pci/devices/0000:05:00.5" # Audio co-processor
+      "pci/devices/0000:05:00.6" # Audio controller
+      "pci/devices/0000:06:00.0" # FCH SATA Controller [AHCI mode]
+      "pci/devices/0000:06:00.1" # FCH SATA Controller [AHCI mode]
+      "pci/devices/0000:06:00.0/ata1" # FCH SATA Controller [AHCI mode]
+      "pci/devices/0000:06:00.1/ata2" # FCH SATA Controller [AHCI mode]
+      "usb/devices/3-3" # USB device 3-3
+      "usb/devices/3-4" # ITE device
+    ];
 
   # systemd.tmpfiles.rules = map
   #         (e:
@@ -278,7 +273,7 @@
     stateVersion = "23.05";
     autoUpgrade = {
       enable = true;
-      channel  = "https://nixos.org/channels/nixos-unstable";
+      channel = "https://nixos.org/channels/nixos-unstable";
     };
   };
 }
