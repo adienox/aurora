138fddd0b96f7fd9f3002b3006474c286a19a3cb
Adienox <adwaitadk@pm.me>
2023-07-19 08:15:52 +0545

    updated code format and changed dynamic pill

--- flake/modules/vscode.nix
+++ flake/modules/vscode.nix
@@ -3,8 +3,13 @@
     enable = true;
     extensions = with pkgs.vscode-extensions; [
       catppuccin.catppuccin-vsc
-      ms-python.python
+      pkief.material-product-icons
+      pkief.material-icon-theme
       vscodevim.vim
+      kahole.magit
+      oderwat.indent-rainbow
+      esbenp.prettier-vscode
+      jnoortheen.nix-ide
       yzhang.markdown-all-in-one
     ];
   };
