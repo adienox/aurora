4feab6db3e3345e54d572e17ed5eb5ddd0a3722f
Adienox <adwaitadk@pm.me>
2023-08-17 09:12:49 +0545

    using custom modification to catppuccin theme

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -49,7 +49,6 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
-(package! catppuccin-theme)
 (package! elfeed-tube)
 (package! elfeed-tube-mpv)
 (package! org-auto-tangle)
