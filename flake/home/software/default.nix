23db5493d21c52a23694c7db46c1d732c087e7d6
Adienox <adwaitadk@pm.me>
2023-08-28 12:10:29 +0545

    boring stuff

--- flake/home/software/default.nix
+++ flake/home/software/default.nix
@@ -46,8 +46,6 @@
       indicator = true;
     };
 
-    # ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"
-    # ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"
     swayosd = {
       enable = true;
       maxVolume = 150;
--- flake/system/software.nix
+++ flake/system/software.nix
@@ -57,6 +57,7 @@
       jack.enable = true;
     };
 
+    # Backlight control for video group
     udev.extraRules = ''
       ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
       ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
