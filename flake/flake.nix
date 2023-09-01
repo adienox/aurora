2d5fe3eb260d0eb2b9c0c360b21ea983a316d60c
Adienox <adwaitadk@pm.me>
2023-09-01 22:48:52 +0545

    added hyprland contrib flake

--- flake/flake.nix
+++ flake/flake.nix
@@ -11,6 +11,11 @@
 
     hyprland.url = "github:hyprwm/Hyprland";
 
+    hyprland-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     spicetify-nix = {
       url = "github:the-argus/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
