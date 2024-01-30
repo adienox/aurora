5868551a1e0967822f01cd97219a8278bd69a996
Adienox <adwaitadk@pm.me>
2024-01-30 12:50:58 +0545

    feat: updating log script to work with new workflow

--- assets/scripts/rofi/obsidian-log.py
+++ assets/scripts/rofi/obsidian-log.py
@@ -1,15 +1,14 @@
 #! /usr/bin/env python3
 
 import datetime
+import os
 import shutil
 import subprocess
 import sys
 from io import TextIOWrapper
 
-daily_folder = "/home/nox/Documents/Knowledge-Garden/Cards/Temporal/Daily/"
-daily_template = (
-    "/home/nox/Documents/Knowledge-Garden/Extras/Templates/Temporal/Daily.md"
-)
+daily_folder = "/home/nox/Documents/garden/Cards/Temporal/Daily/"
+daily_template = "/home/nox/Documents/garden/Extras/Templates/Temporal/daily.md"
 
 # Define the shell command
 command = 'rofi -dmenu -p "Log  " -mesg "<span color=\'#a6e3a1\'><i>Your future self will be proud of you for doing this today!</i></span>" -markup -theme-str "listview {enabled: false;}"'
@@ -20,59 +19,40 @@ def error_send(error: str) -> None:
     sys.exit(error)
 
 
-def open_file(file: str) -> TextIOWrapper:
+def open_file(file: str, template: str) -> TextIOWrapper:
     try:
-        data = open(file, "r+")
+        data = open(file, "a")
         return data
     except FileNotFoundError:
-        shutil.copy(daily_template, file)
-        data = open(file, "r+")
+        shutil.copy(template, file)
+        data = open(file, "a")
         return data
     except Exception as error:
         error_send(str(error))
         sys.exit()
 
 
-def end_of_section(file: list, start_index: int) -> int:
-    for index, line in enumerate(file[start_index + 1 :]):
-        if not line.startswith("-"):
-            return start_index + index + 1
-    return -1
-
-
 def add_to_log(log: str) -> None:
     # Getting date and time
     datetime_now = datetime.datetime.now()
-    date = datetime_now.strftime("%Y-%m-%d.md")
+    file = datetime_now.strftime("%Y-%m-%d.md")
     time = datetime_now.strftime("%H:%M")
 
-    # opening, reading, and putting the file pointer at the start
-    today_file = open_file(daily_folder + date)
-    file_content = today_file.read().split("\n")
-    today_file.seek(0)
-
-    # figuring out the section start and end
-    log_heading = file_content.index("# Log")
-    end_of_log = end_of_section(file_content, log_heading)
-
-    # error handling for section end
-    if end_of_log == -1:
-        error_msg = "End of section log cannot be found"
-        error_send(error_msg)
+    today_file = open_file(os.path.join(daily_folder + file), daily_template)
 
     # writing the file content with new log
-    file_content.insert(end_of_log, f"- **{time}** {log.strip()}")
-    writable_file_content = "\n".join(file_content).strip()
+    writable_log = f"\n- **{time}** {log}"
 
     # writing the file content to file
-    today_file.write(writable_file_content)
-    today_file.truncate()
+    today_file.write(writable_log)
     today_file.close()
 
 
 if __name__ == "__main__":
     # Run the command and capture the output
-    log = subprocess.run(command, shell=True, capture_output=True, text=True)
+    log = subprocess.run(
+        command, shell=True, capture_output=True, text=True
+    ).stdout.strip()
 
-    if not log.stdout.strip() == "":
-        add_to_log(log.stdout.strip())
+    if not log == "":
+        add_to_log(log)
