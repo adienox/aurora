6cb1ab4ad4435a8576709a48ebfe2cbe9072955c
Adienox <adwaitadk@pm.me>
2024-02-02 11:40:33 +0545

    feat: improving mpv experience

--- flake/home/software/media/mpv.nix
+++ flake/home/software/media/mpv.nix
@@ -3,13 +3,6 @@
     mpv = {
       enable = true;
       defaultProfiles = [ "gpu-hq" ];
-      scripts = with pkgs.mpvScripts; [
-        mpris
-        sponsorblock
-        thumbfast
-        webtorrent-mpv-hook
-        uosc
-      ];
 
       bindings = {
         l = "seek 5";
@@ -25,6 +18,8 @@
         screenshot-template = "~/Pictures/mpvscreenshots/%F (%P) %n";
         ytdl-format = "bestvideo[height<=1080]+bestaudio/best[height<=1080]";
         save-position-on-quit = true;
+        write-filename-in-watch-later-config = true;
+        ignore-path-in-watch-later-config = true;
         cache = "yes";
         demuxer-max-bytes = "800M";
         demuxer-max-back-bytes = "200M";
@@ -35,7 +30,40 @@
         cursor-autohide = 1000;
         osc = "no";
         osd-bar = "no";
+        border = "no";
       };
+    };
+  };
+
+  xdg.configFile = {
+    "mpv/scripts/mpris.so".source =
+      "${pkgs.mpvScripts.mpris}/share/mpv/scripts/mpris.so";
+
+    "mpv/scripts/sponsorblock.lua".source =
+      "${pkgs.mpvScripts.sponsorblock}/share/mpv/scripts/sponsorblock.lua";
+
+    "mpv/scripts/thumbfast.lua".source =
+      "${pkgs.mpvScripts.thumbfast}/share/mpv/scripts/thumbfast.lua";
+
+    "mpv/scripts/uosc".source =
+      "${pkgs.mpvScripts.uosc}/share/mpv/scripts/uosc";
+
+    "mpv/scripts/user-input.lua".source = pkgs.fetchurl {
+      url =
+        "https://raw.githubusercontent.com/CogentRedTester/mpv-user-input/40a9fa59d37d7532c055e5c831c1f2b61c6ee741/user-input.lua";
+      hash = "sha256-a0Wfu+qibYQ+AVEqdsOD+wa0DWtb3/HMTdPF2gTeKNA=";
+    };
+
+    "mpv/script-modules/user-input-module.lua".source = pkgs.fetchurl {
+      url =
+        "https://raw.githubusercontent.com/CogentRedTester/mpv-user-input/40a9fa59d37d7532c055e5c831c1f2b61c6ee741/user-input-module.lua";
+      hash = "sha256-qUIA73xU0KWdDAnY69ArwMc80SL5D0qKym7fZK/L+2Y=";
+    };
+
+    "mpv/script-modules/md5.lua".source = pkgs.fetchurl {
+      url =
+        "https://raw.githubusercontent.com/kikito/md5.lua/4b5ce0cc277a5972aa3f5161d950f809c2c62bab/md5.lua";
+      hash = "sha256-ZLkMRdfvNkNW+l82VcN+Ws08zXioCNWKlg768zt0lls=";
     };
   };
 }
