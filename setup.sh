b50c5d5c2a96e632d1adcf44b30f11b530553d60
Adienox <adwaitadk@pm.me>
2024-02-02 11:47:25 +0545

    feat: improved install script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
+echo "Run nixos-rebuild switch --flake .#anomaly to setup os"
+echo "Run nix run home-manager/master -- switch --flake .#nox to setup home config"
 
 if [ ! -d "$XDG_CONFIG_HOME/assets" ]; then
-	ln -s $(pwd)/assets "$XDG_CONFIG_HOME"/assets
+	ln -s "$(pwd)/assets" "$XDG_CONFIG_HOME/assets"
 fi
 
 if [ ! -d "$HOME/.local/bin" ]; then
-	ln -s $(pwd)/assets/scripts/utils "$HOME"/.local/bin
+	ln -s "$(pwd)/assets/scripts/utils" "$HOME/.local/bin"
 fi
 
 if [ ! -d "$XDG_PICTURES_DIR/backgrounds" ]; then
-	ln -s $(pwd)/assets/backgrounds "$XDG_PICTURES_DIR"/backgrounds
+	ln -s "$(pwd)/assets/backgrounds" "$XDG_PICTURES_DIR/backgrounds"
 fi
 
 if [ ! -f "$XDG_CACHE_HOME/background" ]; then
@@ -17,17 +19,22 @@ if [ ! -f "$XDG_CACHE_HOME/background" ]; then
 fi
 
 if [ ! -d "$XDG_CONFIG_HOME/doom" ]; then
-	ln -s $(pwd)/.config/doom "$XDG_CONFIG_HOME"/doom
+	ln -s "$(pwd)/.config/doom" "$XDG_CONFIG_HOME/doom"
 fi
 
 if [ ! -d "$XDG_CONFIG_HOME/zellij" ]; then
-	ln -s $(pwd)/.config/zellij "$XDG_CONFIG_HOME"/zellij
+	ln -s "$(pwd)/.config/zellij" "$XDG_CONFIG_HOME/zellij"
 fi
 
 if [ ! -d "$XDG_CONFIG_HOME/nvim" ]; then
 	git clone https://github.com/NvChad/NvChad "$XDG_CONFIG_HOME"/nvim --depth 1
-	ln -s $(pwd)/.config/nvchad "$XDG_CONFIG_HOME"/nvim/lua/custom
+	ln -s "$(pwd)/.config/nvchad" "$XDG_CONFIG_HOME/nvim/lua/custom"
 fi
 
-echo "Run nixos-rebuild switch --flake .#anomaly to setup os"
-echo "Run nix run home-manager/master -- switch --flake .#nox to setup home config"
+if [ ! -f "$XDG_CONFIG_HOME/mpv/scripts/note.lua" ]; then
+	ln -s "$(pwd)/.config/mpv/note.lua" "$XDG_CONFIG_HOME/mpv/scripts/note.lua"
+fi
+
+if [ ! -f "$XDG_CONFIG_HOME/mpv/scripts/rename.lua" ]; then
+	ln -s "$(pwd)/.config/mpv/rename.lua" "$XDG_CONFIG_HOME/mpv/scripts/rename.lua"
+fi
