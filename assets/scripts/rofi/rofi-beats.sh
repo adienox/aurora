4d67da0acfc59065afd42551e8fe75b2d989467e
Adienox <adwaitadk@pm.me>
2024-02-26 09:28:04 +0545

    feat: updated rofi beats to reflect directory

--- assets/scripts/rofi/rofi-beats.sh
+++ assets/scripts/rofi/rofi-beats.sh
@@ -1,45 +1,30 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 ARGS="--no-resume-playback"
+directory="$HOME/Videos/lofi"
 
-notification(){
+notification() {
 	notify-send "Playing now: " "$@" --icon=media-tape
 }
 
-menu(){
-	printf "1. Chill beats for LEGO building 🧱\n"
-	printf "2. 1 A.M Chill Session 🌌\n"
-	printf "3. 2 A.M Chill Session 🌌\n"
-	printf "4. Earth's History as an Animated Timelapse 🌏\n"
+menu() {
+	# beats=$(ls -1 "$HOME/Videos/lofi/")
+	beats=("$directory"/*)
+	for beat in "${beats[@]}"; do
+		filename="$(basename "$beat")"
+		echo "${filename%.*}"
+	done
 }
 
 main() {
-	choice=$(menu | rofi -dmenu -p 'Lofi 󰎈' | cut -d. -f1)
+	choice=$(menu | rofi -dmenu -p 'Lofi 󰎈')
 
-	case $choice in
-		1)
-			notification "Chill beats for LEGO building 🧱";
-      URL=$HOME/Videos/lofi/Chill\ beats\ for\ LEGO\ building\ 🧱.webm
-			break
-			;;
-		2)
-			notification "1 A.M Chill Session 🌌";
-      URL=$HOME/Videos/lofi/1\ A.M\ Chill\ Session\ 🌌.webm
-			break
-			;;
-		3)
-			notification "2 A.M Chill Session 🌌";
-      URL=$HOME/Videos/lofi/2\ A.M\ Chill\ Session\ 🌌.webm
-			break
-			;;
-		4)
-			notification "Earth's History as an Animated Timelapse ☕️🎶";
-      URL=$HOME/Videos/lofi/Earth\'s\ History\ as\ an\ Animated\ Timelapse.webm
-			break
-			;;
-	esac
-  
-  mpv $ARGS --start=00:40 --title="radio-mpv" "$URL"
+	if [ -n "$choice" ]; then
+		notification "$choice"
+		URL=$(ls "$directory/$choice"*)
+
+		mpv $ARGS --title="radio-mpv" "$URL"
+	fi
 }
 
 pkill -f radio-mpv || main
