32ec9a9a2899b08634c6b6c4a0df44f27c07c516
Adienox <adwaitadk@pm.me>
2024-03-10 20:01:53 +0545

    feat: added layer animations

--- flake/home/desktop/hyprland/config.nix
+++ flake/home/desktop/hyprland/config.nix
@@ -66,6 +66,7 @@
           "windows,1,4,default,slide"
           "border,1,10,default"
           "fade,1,10,default"
+          "layers,1,3,default,fade"
           "workspaces,1,6,default,slide"
         ];
       };
@@ -87,43 +88,10 @@
         animate_manual_resizes = true;
         animate_mouse_windowdragging = true;
         enable_swallow = true;
-        swallow_regex = "^(org.wezfurlong.wezterm|floating)$";
         vfr = true;
         key_press_enables_dpms = true;
         disable_splash_rendering = true;
         disable_hyprland_logo = true;
-      };
-
-      plugin = {
-        hycov = {
-          overview_gappo = 20; # gaps width from screen edge
-          overview_gappi = 14; # gaps width from clients
-          enable_hotarea =
-            1; # enable mouse cursor hotarea, when cursor enter hotarea, it will toggle overview
-          hotarea_monitor =
-            "all"; # monitor name which hotarea is in, default is all
-          hotarea_pos =
-            1; # position of hotarea (1: bottom left, 2: bottom right, 3: top left, 4: top right)
-          hotarea_size = 10; # hotarea size, 10x10
-          swipe_fingers = 4; # finger number of gesture,move any directory
-          move_focus_distance =
-            100; # distance for movefocus,only can use 3 finger to move
-          enable_gesture = 1; # enable gesture
-          auto_exit = 1; # enable auto exit when no client in overview
-          auto_fullscreen =
-            0; # auto make active window maximize after exit overview
-          only_active_workspace = 0; # only overview the active workspace
-          only_active_monitor = 0; # only overview the active monitor
-          enable_alt_release_exit =
-            1; # alt swith mode arg,see readme for detail
-          alt_replace_key =
-            "Super_L"; # alt swith mode arg,see readme for detail
-          alt_toggle_auto_next =
-            0; # auto focus next window when toggle overview in alt swith mode
-          click_in_cursor =
-            1; # when click to jump,the target windwo is find by cursor, not the current foucus window.
-          hight_of_titlebar = 0; # height deviation of title bar height
-        };
       };
     };
   };
