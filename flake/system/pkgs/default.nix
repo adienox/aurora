361185afdca5386e77c16ab60c9a62ad48427349
Adienox <adwaitadk@pm.me>
2024-07-28 21:16:07 +0545

    chore: removing packages

--- flake/system/pkgs/default.nix
+++ flake/system/pkgs/default.nix
@@ -1,8 +1,6 @@
 final: prev: {
   xwaylandvideobridge = prev.libsForQt5.callPackage ./xwaylandvideobridge {};
   win11-icon-theme = prev.callPackage ./win11-icon-theme {};
-  # udiskie = prev.callPackage ./udiskie { };
-  anipy-cli = prev.callPackage ./anipy-cli {};
   mov-cli = prev.callPackage ./mov-cli {};
   apple-fonts = prev.callPackage ./fonts/apple-fonts.nix {};
 }
