f1767d95beef25f250ef483af4812a9fa2364118
Adienox <adwaitadk@pm.me>
2024-08-22 19:42:06 +0545

    feat: simplified script

--- assets/scripts/hypr/screenrecord.sh
+++ assets/scripts/hypr/screenrecord.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 VIDEO="$XDG_VIDEOS_DIR/recordings/$(date +%Y-%m-%d_%H-%M-%S).mp4"
 
-fuser -sk -SIGINT "$0"
-test "$1" = off && exit 0
-[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -enx "$0" "$0" "$@" || :
-exec wf-recorder -f "$VIDEO"
+if pgrep -x wf-recorder >/dev/null; then
+	kill $(pgrep -x wf-recorder)
+else
+	wf-recorder -f "$VIDEO"
+fi
