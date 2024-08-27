d4ff1625c111848737271880ef04d655473dd3d3
Adienox <adwaitadk@pm.me>
2024-08-27 23:50:30 +0545

    chore: housekeeping

--- flake/home/theme/qt.nix
+++ flake/home/theme/qt.nix
@@ -1,24 +1,7 @@
-{pkgs, ...}:
-# Qt theming with Kvantum
-{
+{...}: {
   qt = {
     enable = true;
     platformTheme.name = "kvantum";
     style.name = "kvantum";
   };
-
-  # home.packages = with pkgs; [
-  #   libsForQt5.qtstyleplugin-kvantum
-  #   (catppuccin-kvantum.override {
-  #     accent = "Peach";
-  #     variant = "Mocha";
-  #   })
-  # ];
-
-  # home.sessionVariables = { QT_STYLE_OVERRIDE = "kvantum"; };
-  #
-  # xdg.configFile."Kvantum/kvantum.kvconfig".source =
-  #   (pkgs.formats.ini { }).generate "kvantum.kvconfig" {
-  #     General.theme = "Catppuccin-Mocha-Peach";
-  #   };
 }
