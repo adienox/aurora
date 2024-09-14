54c0e946ef248997230ef8d47c48088667b52691
Adienox <adwaitadk@pm.me>
2024-09-14 17:53:50 +0545

    refactor: move tmux related plugins

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -82,43 +82,4 @@ return {
     event = 'InsertEnter',
     opts = {},
   },
-
-  {
-    'christoomey/vim-tmux-navigator',
-    cmd = {
-      'TmuxNavigateLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-    },
-    keys = {
-      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-    },
-  },
-
-  {
-    'samharju/yeet.nvim',
-    dependencies = {
-      'stevearc/dressing.nvim', -- optional, provides sane UX
-    },
-    cmd = 'Yeet',
-    opts = {
-      notify_on_success = false,
-    },
-    keys = {
-      {
-        '\\\\',
-        mode = { 'n' },
-        function()
-          require('yeet').execute()
-        end,
-        desc = 'Yeet Execute',
-      },
-    },
-  },
 }
