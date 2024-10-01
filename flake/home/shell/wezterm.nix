0b4b07dc3aba7982d6f1bfd0ee74bd160b8950ab
Adienox <adwaitadk@pm.me>
2024-10-01 20:04:55 +0545

    feat: update wezterm config

--- flake/home/shell/wezterm.nix
+++ flake/home/shell/wezterm.nix
@@ -1,74 +1,36 @@
-_: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   programs.wezterm = {
-    enable = true;
-    # package = inputs.self.packages.${pkgs.system}.wezterm;
+    enable = false;
+    package = inputs.wezterm.packages.${pkgs.system}.default;
     extraConfig = ''
-      local wezterm = require ("wezterm")
+      local wezterm = require("wezterm")
 
-      local function font_with_fallback(name, params)
-        local names = { name, "Material Symbols Outlined"}
-        return wezterm.font_with_fallback(names, params)
-      end
+      config = wezterm.config_builder()
 
-      local font_name = "CaskaydiaCove Nerd Font"
+      config = {
+      	automatically_reload_config = true,
+      	enable_tab_bar = false,
+      	warn_about_missing_glyphs = false,
+      	enable_scroll_bar = false,
+      	window_background_opacity = 0.7,
+      	scrollback_lines = 10000,
+      	enable_wayland = true,
+      	check_for_updates = false,
+      	default_cursor_style = "BlinkingBar",
+      	window_close_confirmation = "NeverPrompt",
+      	color_scheme = "Catppuccin Mocha (Gogh)",
+      	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" }),
+      	font_size = 12.5,
+      	colors = {
+      		background = "#000000",
+      	},
+      }
 
-      return {
-
-        colors = {
-          background = "#131313"
-        },
-
-        font_size = 10,
-
-       -- Font config
-        font = font_with_fallback(font_name),
-        font_rules = {
-           {
-             italic = true,
-             font = font_with_fallback(font_name, { italic = true }),
-           },
-           {
-             italic = false,
-             font = font_with_fallback(font_name, { bold = true }),
-           },
-           {
-             intensity = "Bold",
-             font = font_with_fallback(font_name, { bold = true }),
-           },
-         },
-
-        color_scheme = "Gruvbox Material (Gogh)",
-        window_background_opacity = 1.0,
-        warn_about_missing_glyphs = false,
-        enable_scroll_bar = false,
-        enable_tab_bar = false,
-        scrollback_lines = 10000,
-        window_padding = {
-          left = 25,
-          right = 25,
-          top = 25,
-          bottom = 25,
-        },
-        enable_wayland = true,
-        check_for_updates = false,
-        default_cursor_style = "SteadyBar",
-        automatically_reload_config = true,
-
-        disable_default_key_bindings = true,
-        keys = {
-          {
-            key = "v",
-            mods = "CTRL|SHIFT",
-            action = wezterm.action({ PasteFrom = "Clipboard" }),
-          },
-          {
-            key = "c",
-            mods = "CTRL|SHIFT",
-            action = wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }),
-          },
-          },
-            window_close_confirmation = "NeverPrompt",
-          }
+      return config
     '';
   };
 }
