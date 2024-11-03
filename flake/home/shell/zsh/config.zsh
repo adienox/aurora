39faacfacefa431d550bf5d7e917350f4582df4b
Adienox <adwaitadk@pm.me>
2024-11-03 14:41:34 +0545

    feat: update zsh config

---
+++ flake/home/shell/zsh/config.zsh
@@ -0,0 +1,36 @@
+setopt interactivecomments # allow comments in interactive mode
+setopt magicequalsubst     # enable filename expansion for arguments of the form 'anything=expression'
+setopt nonomatch           # hide error message if there is no match for the pattern
+setopt notify              # report the status of background jobs immediately
+setopt numericglobsort     # sort filenames numerically when it makes sense
+setopt promptsubst         # enable command substitution in prompt
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias tb="curl -F "file=@-" gcg.sh"
+
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
+vterm_printf() {
+    if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ]); then
+        # Tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+    elif [ "${TERM%%-*}" = "screen" ]; then
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$1"
+    else
+        printf "\e]%s\e\\" "$1"
+    fi
+}
+if [[ "$INSIDE_EMACS" = 'vterm' ]]; then
+    alias clear='vterm_printf "51;Evterm-clear-scrollback";tput clear'
+fi
+vterm_prompt_end() {
+    vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+}
+setopt PROMPT_SUBST
+PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
--- flake/home/shell/zsh/default.nix
+++ flake/home/shell/zsh/default.nix
@@ -2,7 +2,8 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   xdg.configFile."zsh/modules/termsupport.zsh".source = ./termsupport.zsh;
 
   programs.zsh = {
@@ -16,7 +17,10 @@
     history = {
       extended = true;
       ignoreAllDups = true;
-      ignorePatterns = ["rm *" "pkill *"];
+      ignorePatterns = [
+        "rm *"
+        "pkill *"
+      ];
       path = "$ZDOTDIR/.zsh_history";
       save = 1000000;
       size = 1000000;
@@ -89,47 +93,17 @@
     };
 
     initExtra = ''
-      setopt interactivecomments # allow comments in interactive mode
-      setopt magicequalsubst     # enable filename expansion for arguments of the form 'anything=expression'
-      setopt nonomatch           # hide error message if there is no match for the pattern
-      setopt notify              # report the status of background jobs immediately
-      setopt numericglobsort     # sort filenames numerically when it makes sense
-      setopt promptsubst         # enable command substitution in prompt
-
       # run programs that are not in PATH with comma
       command_not_found_handler() {
-        ${pkgs.comma}/bin/comma "$@"
+          ${pkgs.comma}/bin/comma "$@"
       }
-
-      alias ..='cd ..'
-      alias ...='cd ../..'
-      alias ....='cd ../../..'
-      alias .....='cd ../../../..'
-      alias ......='cd ../../../../..'
-      alias tb="curl -F "file=@-" gcg.sh"
-
-      alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
-      alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
-
-      # runs fetch if shell is interactive and not inside vscode and nvim
-      # if [[ $- == *i* && "$TERM_PROGRAM" != "vscode" && -z "$VIM" ]] ; then
-      #   #$XDG_CONFIG_HOME/assets/scripts/fetch.sh
-      #   maxfetch
-      # fi
-
+      ${builtins.readFile ./config.zsh}
       # Plugins
       source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
-      # source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
       source ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
       source ${config.xdg.configHome}/zsh/modules/termsupport.zsh
-
-      # press <C-v><UP> and <C-v><DOWN> at your ZSH command line prompt for correct key codes.
-      # bindkey '^[OA' history-substring-search-up
-      # bindkey '^[OB' history-substring-search-down
-
-      # bindkey -M vicmd 'k' history-substring-search-up
-      # bindkey -M vicmd 'j' history-substring-search-down
       export LS_COLORS=$(${pkgs.vivid}/bin/vivid generate catppuccin-mocha)
     '';
+
   };
 }
