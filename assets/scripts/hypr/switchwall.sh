cdd7d9a9472c09c2f6a534a8df12237574a3a80a
Adienox <adwaitadk@pm.me>
2024-11-17 20:37:51 +0545

    feat: remove transition from cursor position

--- assets/scripts/hypr/switchwall.sh
+++ assets/scripts/hypr/switchwall.sh
@@ -2,14 +2,10 @@
 
 cd "$HOME"/Pictures/wallpapers || exit
 imgpath=$(yad --width 1000 --height 600 --file --add-preview --large-preview --title='Choose wallpaper')
-screensizey=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
-cursorposx=$(hyprctl cursorpos -j | gojq '.x')
-cursorposy=$(hyprctl cursorpos -j | gojq '.y')
-cursorposy_inverted=$((screensizey - cursorposy))
 
 if [ "$imgpath" == '' ]; then
-	echo 'Aborted'
-	exit 0
+    echo 'Aborted'
+    exit 0
 fi
 
 rm "$XDG_CACHE_HOME"/background
@@ -17,5 +13,4 @@ ln -s "$imgpath" "$XDG_CACHE_HOME"/background
 
 # Change swww wallpaper
 swww img "$imgpath" --transition-step 230 --transition-fps 60 \
-	--transition-type grow --transition-angle 30 --transition-duration 1 \
-	--transition-pos "$cursorposx, $cursorposy_inverted" &
+    --transition-type grow --transition-angle 30 --transition-duration 1 &
